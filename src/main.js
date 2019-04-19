af896fdf89ca0f7d35b22c44d71f5cc74bcb60af
IvanEdward <daodaolv7@126.com>
2019-04-19 15:07:56 +0800

    上传home

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import store from './store'
 import '@/assets/css/public.css'
 
 // ↓ ------ 引入 ElementUi 插件 -------
-// import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
-// Vue.use(ElementUI)
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -27,6 +27,7 @@
 <script>
     // @ is an alias to /src
     // import HelloWorld from '@/components/HelloWorld.vue'
+    // import
     export default {
         name: 'home',
         components: {
