b8a64abe5b2da1a5c14e027127b7204f9eba4ddd
zhouguiquan <946529611@qq.com>
2019-04-19 11:45:31 +0800

    增加一个模板

---
+++ src/components/furniture/FurnitureHeaderIcon.vue
@@ -0,0 +1,15 @@
+<template>
+       <div class="f-category">
+        <div class="furniture-title">
+            <!-- 图标 -->
+            <div class="furniture-icon">
+
+                <i class="f-icon-kitchen f-icon">
+                    <img src="" alt="">
+                </i>
+            </div>
+            <div class="title f-font-t">
+            </div>
+        </div>
+        </div>
+</template>
