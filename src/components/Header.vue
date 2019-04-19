6c46721597c3c10779a5e87108c2d029cdf092c5
wuyu <844658509@qq.com>
2019-04-19 12:45:46 +0800

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,29 @@
 <template>
     <header class="header">
-        <div class="header-title">生长于野 <span class="separator">·</span> 安于室</div>
-            <div class="logo"></div>
-            <nav>
-                <ul class="header-nav">
+        <div class="header_title">
+            <div class="head_text_box">
+                <span>生长于野 · 安于室</span>
+            </div>
+        </div>
+        <div class="head_nav_box">
+            <div class="head_nav">
+                <div class="home_logo"></div>
+                <ul class="clear">
                     <li><router-link to="/home" >主页</router-link></li>
                     <li><router-link to="/furniture?1">家具</router-link></li>
                     <li><router-link to="/furnishing">家居</router-link></li>
                     <li><router-link to="/lifestyle">风格</router-link></li>
                     <li><router-link to="/afflatus">灵感</router-link></li>
                 </ul>
-            </nav>
-            <div class="header-right">
-                <div class="input-hover">
-                    <input type="text" class="u-search-input">
-                    <i class="iconfont icon-sousuo"></i>
-                </div>
+                <span class="head_nav_button">
+
+                    <input type="text" value="请输入需要查询的内容"><span class="iconfont icon-fangdajing"></span>
+                    <span><router-link to="/shoppingCart"><span class="iconfont icon-gouwuchekong"></span></router-link></span>
+                    <span><router-link to="/about"><span class="iconfont icon-dingdan"></span></router-link></span>
+                    <span ><router-link to="/personal"><span class="iconfont icon-denglu"></span></router-link></span>
+                </span>
             </div>
+        </div>
     </header>
 </template>
 
@@ -36,216 +43,84 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    html,body{
-        width: 100%;
-    }
-    body{
-        color: #000;
-        font-weight: 500;
-        font-size: 1rem;
-        line-height: 1.5;
-        background-color: #fff;
-        font-family: '方正宋刻本秀楷';
-    }
-    /*@font-face{*/
-        /*font-family: '方正宋刻本秀楷';*/
-        /*src: url('/fonts/FZSKBXKK.eot');*/
-        /*src: local('/fonts/FZSKBXKK Regular'),*/
-        /*local('/fonts/FZSKBXKK'),*/
-        /*url('/fonts/FZSKBXKK.ttf') format('truetype'),*/
-        /*url('/fonts/FZSKBXKK.svg#FZSKBXKK') format('svg');*/
-    /*}*/
-
-    ul,li{
-        list-style: none;
-    }
-    a{
-        color: #666;
-        font-family: '方正宋刻本秀楷';
-        text-decoration: none;
-    }
-    a:hover{
-        color: #000;
-        font-family: '方正宋刻本秀楷';
-        text-decoration: none;
-    }
-    .center{
-        width: 1100px;
-        margin: 0 auto;
-    }
-    .clear::before,
-    .clear::after{
-        content: '';
-        display: table;
-    }
-    .clear::after{
-        clear: both;
-    }
-
-    .header-title{
-        font-size: 12px;
-        color: #999;
-        text-align: center;
-        background-color: #f7f7f7;
-        min-width: 1200px;
-    }
-    .separator{
-        font-size: 20px;
-        vertical-align: middle;
-    }
-    .header{
-        border-bottom: 1px solid #cfcdcd;
-        width: 100%;
-        min-width: 1200px;
-        background-color: #fff;
-        z-index: 999;
-    }
-    .logo{
-        float: left;
-        width: 62px;
-        height: 83px;
-        /*background: url(../images/chart/logo.png) no-repeat center;*/
-        background-size: cover;
-        line-height: 83px;
-        margin-right: 180px;
-    }
-    /* header-nav */
-    .header-right{
-        float: right;
-        line-height: 83px;
-    }
-    .header-nav li{
-        float: left;
-        padding: 0 40px;
-    }
-    .header-nav a{
+    a{color:#000;}
+    a:link{color:#000000}
+    a:hover{color:#eeeeee}
+    a{text-decoration:none;}
+    .clear:after{
+        content: "";
         display: block;
-        line-height: 83px;
-        height: 83px;
-        font-size: 16px;
-    }
-    .cur {
-        border-bottom:2px solid #000;
-    }
-    .header-nav  li:hover a{
-        border-bottom:2px solid #000 !important;
-    }
-    /* header-right */
-    .header-right div{
-        float: left;
-        padding: 0 5px;
-        color: #666;
-    }
-    .header-right span{
-        cursor: pointer;
-    }
-    .header-right .iconfont{
-        font-size: 18px;
-    }
-    .u-search-input{
-        border: 1px solid #a5a4a4;
-        width: 100px;
-        height: 20px;
-        border-radius: 1em;
-        padding: 0 5px;
-        font-size: 12px;
-        line-height: 20px;
-        vertical-align: middle;
-        display: none;
-        margin-right: 2px;
-        overflow: visible;
-        outline: none;
-    }
-    .input-hover:hover input{
-        display:inline-block;
-    }
-    .item{
-        margin: 0 5px;
-    }
-    .hover-text{
-        display: none;
-        vertical-align: middle;
-        font-size: 11px;
-        color: #666;
-    }
-
-
-    .nickname-item{
-        position: relative;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        cursor: pointer;
-    }
-    .iconimg{
-        width: 18px;
-        height: 18px;
-        vertical-align: middle;
-    }
-    #userbox{
+        clear: both;
+        height: 0;
         visibility: hidden;
-        position: absolute;
-        top: 100%;
-        right: 0;
-        margin-top: 2px;
-        border: 1px solid #c9c9c9;
-        width: 200px;
-        line-height: 1.2;
-        background-color: #fff;
-        padding: 0 !important;
-        z-index: 998;
     }
-    .userbox_top{
+
+    .header_title{
         width: 100%;
-        box-sizing: border-box;
-        padding: 25px !important;
-        display: flex;
-        align-self: auto;
-    }
-    .userbox_top span{
-        line-height: 30px;
-        font-size: 14px;
-        padding-left: 25px;
-    }
-    .userbox_top img{
-        width: 30px;
+        background-color: #fffac9;
         height: 30px;
-        border-radius: 50%;
+        overflow: hidden;
     }
-    .userbox_bottom{
-        display: inline-block;
-        width: 100%;
-        font-size: 12px;
-        line-height: 33px;
+    .head_text_box{
+        width: 1100px;
+        margin-top:5px;
+        margin-left: auto;
+        margin-right: auto;
         text-align: center;
-        color: #666;
-        padding: 0;
-        user-select: none;
-        font-weight: 400;
-        padding: 0 !important;
-        display: flex;
-        justify-content: space-between
     }
-    .userbox_bottom a{
-        display: inline-block;
-        box-sizing: border-box;
-        width: 50%;
-        border-top: 1px solid #c9c9c9;
+    .head_nav ul li{
+        float: left;
+        list-style: none;
+        display: inline;
+        padding: 35px  0 31px 0;
+        margin: 0 30px;
+        color: #bcbcbc;
+
     }
-    .userbox_bottom a:first-child{
-        border-right: 1px solid #c9c9c9;
+    .head_nav ul li:hover{
+        border-bottom: 3px #000 solid;
     }
-    .nickname{
-        width: 35px;
-        font-size: 12px;
-        overflow:hidden;
-        text-overflow:ellipsis;
-        white-space:nowrap;
+    .head_nav ul{
+        height: 90px;
+        width: 460px;
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+
     }
+    .home_logo{
+        float: left;
+        width: 70px;
+        height: 90px;
+        background-color: #000;
+    }
+    .head_nav{
+        width: 1100px;
+        margin-left: auto;
+        margin-right: auto;
+        position: relative;
+
+    }
+    .head_nav_box{
+        background-color: #d9d9d9;
+        width: 100%;
+
+    }
+    .head_nav_button{
+        position: absolute;
+        top:36px;
+        right: 20px;
+    }
+    .head_nav_button span{
+        margin-left: 10px;
+    }
+    .head_nav_button span :nth-child(2){
+        margin-left: 0px;
+    }
+
+    .head_nav_button input{
+        width: 80px;
+    }
+
 
 </style>
