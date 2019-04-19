186b7d5c884bfef2f50df53e325d0edf86cc7b1f
wuyu <844658509@qq.com>
2019-04-19 10:42:08 +0800

    app，main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/Css/public.css'
+import '@/assets/css/public.css'
 
 Vue.config.productionTip = false;
 
